16fa70b158c9e29ef696ef8eab7510e607c867d0
Yukai OUYANG <1102226419@qq.com>
2024-12-11 15:32:15 +0800

    1

---
+++ src/test/java/com/learning/versioncontroll/VersionControllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.versioncontroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionControllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
